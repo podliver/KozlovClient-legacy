ee40ff8b5d32cf3dc5f520317a449e08666ce683
podliver <sqdkaeel@mail.ru>
2024-07-01 04:49:44 +0900

    Update bipas.java

--- src/main/java/cn/ponos/podliver/kozlov/mod/client/notclient/bypass/balls/best/software/new/extra/bruteforce/interpolation/prediction/everything/minecraft/dev/fix/branch/fast/async/boost/KozlovClient/modules/player/reach/bipas.java
+++ src/main/java/cn/ponos/podliver/kozlov/mod/client/notclient/bypass/balls/best/software/new/extra/bruteforce/interpolation/prediction/everything/minecraft/dev/fix/branch/fast/async/boost/KozlovClient/modules/player/reach/bipas.java
@@ -1,6 +1,9 @@
 class bipas {
     // finding a flaw in the anticheat, so you can use 5 blocks reach.
     // credits to: KOZLOVBypasser.
+    public static findbypas() {
+        return;
+    }
     public static void main(String[] args) {
         System.out.println(bypas=true); 
                 System.out.println(bypas=true); 
