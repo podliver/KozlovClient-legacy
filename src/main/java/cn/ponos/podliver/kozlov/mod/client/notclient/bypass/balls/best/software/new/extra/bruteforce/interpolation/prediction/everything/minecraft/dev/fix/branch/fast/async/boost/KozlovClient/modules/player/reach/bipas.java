b6f20db954293b5fb26c3a222c60c9c27a4f246c
podliver <sqdkaeel@mail.ru>
2024-07-01 04:48:16 +0900

    Update bipas.java

--- src/main/java/cn/ponos/podliver/kozlov/mod/client/notclient/bypass/balls/best/software/new/extra/bruteforce/interpolation/prediction/everything/minecraft/dev/fix/branch/fast/async/boost/KozlovClient/modules/player/reach/bipas.java
+++ src/main/java/cn/ponos/podliver/kozlov/mod/client/notclient/bypass/balls/best/software/new/extra/bruteforce/interpolation/prediction/everything/minecraft/dev/fix/branch/fast/async/boost/KozlovClient/modules/player/reach/bipas.java
@@ -1,5 +1,56 @@
 class bipas {
+    // finding a flaw in the anticheat, so you can use 5 blocks reach.
+    // credits to: KOZLOVBypasser.
     public static void main(String[] args) {
         System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+                System.out.println(bypas=true); 
+        
     }
 }
