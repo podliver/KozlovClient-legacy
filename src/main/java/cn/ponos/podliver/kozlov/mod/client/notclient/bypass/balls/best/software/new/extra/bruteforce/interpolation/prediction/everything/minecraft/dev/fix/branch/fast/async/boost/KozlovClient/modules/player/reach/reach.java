5fbf6acca771615149451582efd3e674b4f99a06
podliver <sqdkaeel@mail.ru>
2024-07-01 04:50:49 +0900

    Update reach.java

--- src/main/java/cn/ponos/podliver/kozlov/mod/client/notclient/bypass/balls/best/software/new/extra/bruteforce/interpolation/prediction/everything/minecraft/dev/fix/branch/fast/async/boost/KozlovClient/modules/player/reach/reach.java
+++ src/main/java/cn/ponos/podliver/kozlov/mod/client/notclient/bypass/balls/best/software/new/extra/bruteforce/interpolation/prediction/everything/minecraft/dev/fix/branch/fast/async/boost/KozlovClient/modules/player/reach/reach.java
@@ -1,6 +1,12 @@
+import KozlovClient.src.main.java.cn.ponos.podliver.kozlov.mod.client.notclient.bypass.balls.best.software.new.extra.bruteforce.interpolation.prediction.everything.minecraft.dev.fix.branch.fast.async.boost.KozlovClient.modules.player.reach.bipas;
+
 class reach {
+    // just sets your reach to 5 using the bipas method by KOZLOVBypasser.
     public static void main(String[] args) {
         System.out.println("!"); 
     }
-	system.ponos.reach=5
+	boolean bypass = bipas.findbypas();
+	if (bipas) {
+		system.ponos.reach=5
+	}
 }
